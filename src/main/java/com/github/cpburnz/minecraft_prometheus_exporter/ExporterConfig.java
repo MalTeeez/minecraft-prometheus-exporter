6f909fd61f029903e2905c92d1e78f497d4b7e19
Caedis <Caedis@users.noreply.github.com>
2025-01-20 14:15:13 -0600

    Reenabling spotless
    Major refactor; dont plan on upstreaming it

---
+++ src/main/java/com/github/cpburnz/minecraft_prometheus_exporter/ExporterConfig.java
@@ -0,0 +1,11 @@
+package com.github.cpburnz.minecraft_prometheus_exporter;
+
+/**
+ * The Config class defines the mod config. This is used to load and generate
+ * the "prometheus_exporter.cfg" config file.
+ */
+public class ExporterConfig {
+
+	public static String listen_address;
+	public static int listen_port;
+}
